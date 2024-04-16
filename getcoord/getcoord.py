4df6ceb38d75b07e0dfd9216d643e56a16e6f36b
EfimPechersky <81502368+EfimPechersky@users.noreply.github.com>
2024-04-16 10:10:40 +0800

    Update getcoord.py

--- getcoord/getcoord.py
+++ getcoord/getcoord.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-def get_coords(text):##Функция делает запрос в поиске по организациям
+def get_coords(text,api_key):##Функция делает запрос в поиске по организациям
   request = requests.get("https://search-maps.yandex.ru/v1/?text="\
                          +text+"&lang=ru_RU&results=100&\
-                        apikey=YOUR_API_KEY")
+                        apikey="+api_key)
   place = json.loads(request.content)
   return place
