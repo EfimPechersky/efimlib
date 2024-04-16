954e80e4f63fe9b28328d1f48fe6dce17eff4ef4
EfimPechersky <81502368+EfimPechersky@users.noreply.github.com>
2024-04-16 10:20:08 +0800

    Update test_getcoord.py

--- test/test_getcoord.py
+++ test/test_getcoord.py
@@ -3,6 +3,6 @@ from getcoord.getcoord import get_coords
 
 def test_api():
   townname="Москва"
-  place=get_coords(townname)
-  assert (townname in place['features'][0]['properties']['description'])
+  place=get_coords(townname,"somekey")
+  assert ("Key is required" == place['message'])
   
